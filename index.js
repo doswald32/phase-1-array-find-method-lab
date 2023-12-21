66f44013a8a38578f993c77964b68cd1ab34d168
doswald32 <dan.oswald32@gmail.com>
2023-12-20 18:06:06 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+/*
+function superbowlWin(array) {
+    for (let season in array) {
+        if (season.result === "W") {
+            return season.year;
+        }
+    }
+}
+*/
+
+function superbowlWin(array) {
+    const season = array.find((element) => element.result === "W");
+    return season ? season.year : undefined;
+}
